97a36c3b463dcd525c8f08820208c5c1eecb662a
Henry Yeary <henmofb@gmail.com>
2023-03-14 11:31:10 -0500

    Added some substance to the views as well as seed-data for testing.

--- hours_per_week_calculator.js
+++ hours_per_week_calculator.js
@@ -33,20 +33,51 @@ Should divide the number of total hours by 6 times the current number of weeks l
 
 const express = require("express");
 const morgan = require("morgan");
+const Strategy = require('./lib/strategies');
 
 const app = express();
 const host = "localhost";
 const port = 3000;
 
+let strats = require('./lib/seed-data');
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+const sortStrategies = strats => {
+  return strats.slice().sort((stratA, stratB) => {
+    let titleA = stratA.title.toLowerCase();
+    let titleB = stratB.title.toLowerCase();
+
+    if (titleA < titleB) {
+      return -1;
+    } else if (titleA > titleB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
 
 app.get("/", (req, res) => {
-    res.render("main-page");
+    res.render("strategies", {
+      strats: sortStrategies(strats),
+    });
+});
+
+app.get("/strategies/new", (req, res) => {
+  res.render("new-strategy");
+});
+
+app.post("/strategies", (req, res) => {
+  let title = req.body.stratTitle.trim();
+  strats.push(new Strategy(title));
 });
 
 app.listen(port, host, () => {
   console.log(`Todos is listening on port ${port} of ${host}!`);
-})
+});
